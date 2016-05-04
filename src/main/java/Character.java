c6ea827d7bb2da3dc83564a0f4fecfb091cd72a1
Taffy Cheng <taffy128s@gmail.com>
2016-05-04 16:20:48 +0800

    Add nodes.

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import de.looksgood.ani.Ani;
 import processing.core.PApplet;
 
 /**
@@ -8,18 +9,47 @@ import processing.core.PApplet;
 */
 public class Character {
 	
+	public float x, y, radius = 35;
 	private MainApplet parent;
 	private String name;
-	
+	private int color;
+	boolean mouseOver, locked, inCircle;
+	private Ani ani;
 
-	public Character(MainApplet parent){
+	public Character(MainApplet parent, String name, String color, float x, float y, 
+			boolean mouseOver, boolean locked, boolean inCircle) {
 
 		this.parent = parent;
+		this.name = name;
+		this.color = (int) Long.parseLong(color.replace("#", ""), 16);
+		this.x = x;
+		this.y = y;
+		this.mouseOver = mouseOver;
+		this.locked = locked;
+		this.inCircle = inCircle;
+		Ani.init(this.parent);
 		
 	}
 
-	public void display(){
-
+	public void display() {
+		parent.fill(color);
+		parent.noStroke();
+		parent.ellipse(x, y, radius, radius);
+		if (inCircle) {
+			
+		}
+		
+	}
+	
+	public void showLabelAndBigger() {
+		if (PApplet.dist(x, y, parent.mouseX, parent.mouseY) < 35 / 2) {
+			ani = Ani.to(this, (float) 0.5, "radius", 50);
+			parent.fill(0, 162, 123);
+			parent.rect(parent.mouseX, parent.mouseY -  15, name.length() * 15, 30, 15);
+			parent.fill(255);
+			parent.textSize(16);
+			parent.text(this.name, parent.mouseX + 10, parent.mouseY + 5);
+		} else ani = Ani.to(this, (float) 0.5, "radius", 35);
 	}
 	
 }
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,5 +1,8 @@
 package main.java;
 
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 import processing.data.JSONArray;
 import processing.data.JSONObject;
@@ -16,6 +19,8 @@ public class MainApplet extends PApplet{
 	private JSONObject[] datas = new JSONObject[8];
 	private JSONArray[] nodes = new JSONArray[8];
 	private JSONArray[] links = new JSONArray[8];
+	private int version = 1;
+	private ArrayList<Character> characters = new ArrayList<Character>();
 	
 	private final static int width = 1200, height = 650;
 	
@@ -24,17 +29,56 @@ public class MainApplet extends PApplet{
 		size(width, height);
 		smooth();
 		loadData();
+		loadNodeAndLink();
 		
 	}
 
 	public void draw() {
+		// Draw the background and show the nodes.
+		background(255);
+		for (Character c : characters) {
+			c.display();
+		}
+		for (Character c : characters) {
+			c.showLabelAndBigger();
+		}
 	}
 
+	public void keyPressed() {
+		// Change the versions.
+		if (keyCode == KeyEvent.VK_1) version = 1;
+		else if (keyCode == KeyEvent.VK_2) version = 2;
+		else if (keyCode == KeyEvent.VK_3) version = 3;
+		else if (keyCode == KeyEvent.VK_4) version = 4;
+		else if (keyCode == KeyEvent.VK_5) version = 5;
+		else if (keyCode == KeyEvent.VK_6) version = 6;
+		else if (keyCode == KeyEvent.VK_7) version = 7;
+		loadNodeAndLink();
+	}
+	
 	private void loadData(){
+		// Load in all data.
 		for (int i = 1; i <= 7; i++) {
 			datas[i] = loadJSONObject(path + fileFront + Integer.toString(i) + fileBack);
 			nodes[i] = datas[i].getJSONArray("nodes");
 			links[i] = datas[i].getJSONArray("links");
+		}
+	}
+	
+	private void loadNodeAndLink() {
+		characters.clear();
+		int x = 0, y = 0;
+		for (int i = 0; i < nodes[version].size(); i++) {
+			// When the node reaches the end, it will go back to the top.
+			if (i % 11 == 0) {
+				x = 35 + i * 5;
+				y = 48;
+			}
+			JSONObject obj = nodes[version].getJSONObject(i);
+			Character c = new Character(this, obj.getString("name"), obj.getString("colour"), x, y, false, false, false);
+			characters.add(c);
+			// The gap between the nodes.
+			y += 55;
 		}
 	}
 
