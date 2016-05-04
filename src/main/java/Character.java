54707ca1531d259d0579f20ff66dca599e28f60c
Taffy Cheng <taffy128s@gmail.com>
2016-05-04 18:39:15 +0800

    Prepared to move the circles to the 'Big Circle'.

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,55 +1,48 @@
 package main.java;
 
-import de.looksgood.ani.Ani;
-import processing.core.PApplet;
-
 /**
 * This class is used to store states of the characters in the program.
 * You will need to declare other variables depending on your implementation.
 */
 public class Character {
-	
-	public float x, y, radius = 35;
+	public static int DIAMETER = 35;
+	public float x, y;
+	private float originX, originY, diameter = DIAMETER;
 	private MainApplet parent;
 	private String name;
 	private int color;
-	boolean mouseOver, locked, inCircle;
-	private Ani ani;
+	private boolean inCircle;
 
-	public Character(MainApplet parent, String name, String color, float x, float y, 
-			boolean mouseOver, boolean locked, boolean inCircle) {
-
+	public Character(MainApplet parent, String name, String color, float x, float y) {
 		this.parent = parent;
 		this.name = name;
 		this.color = (int) Long.parseLong(color.replace("#", ""), 16);
+		this.originX = x;
+		this.originY = y;
 		this.x = x;
 		this.y = y;
-		this.mouseOver = mouseOver;
-		this.locked = locked;
-		this.inCircle = inCircle;
-		Ani.init(this.parent);
-		
 	}
 
 	public void display() {
 		parent.fill(color);
 		parent.noStroke();
-		parent.ellipse(x, y, radius, radius);
+		parent.ellipse(x, y, diameter, diameter);
 		if (inCircle) {
 			
+		} else {
+			
 		}
-		
 	}
 	
-	public void showLabelAndBigger() {
-		if (PApplet.dist(x, y, parent.mouseX, parent.mouseY) < 35 / 2) {
-			ani = Ani.to(this, (float) 0.5, "radius", 50);
-			parent.fill(0, 162, 123);
-			parent.rect(parent.mouseX, parent.mouseY -  15, name.length() * 15, 30, 15);
-			parent.fill(255);
-			parent.textSize(16);
-			parent.text(this.name, parent.mouseX + 10, parent.mouseY + 5);
-		} else ani = Ani.to(this, (float) 0.5, "radius", 35);
+	public String getName() {
+		return name;
 	}
 	
+	public float getOriginX() {
+		return originX;
+	}
+	
+	public float getOriginY() {
+		return originY;
+	}
 }
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -3,6 +3,7 @@ package main.java;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
+import de.looksgood.ani.Ani;
 import processing.core.PApplet;
 import processing.data.JSONArray;
 import processing.data.JSONObject;
@@ -11,6 +12,7 @@ import processing.data.JSONObject;
 * This class is for sketching outcome using Processing
 * You can do major UI control and some visualization in this class.  
 */
+
 @SuppressWarnings("serial")
 public class MainApplet extends PApplet{
 	private String path = "main/resources/";
@@ -19,31 +21,72 @@ public class MainApplet extends PApplet{
 	private JSONObject[] datas = new JSONObject[8];
 	private JSONArray[] nodes = new JSONArray[8];
 	private JSONArray[] links = new JSONArray[8];
-	private int version = 1;
 	private ArrayList<Character> characters = new ArrayList<Character>();
+	private int version = 1;
+	private Character lockWhich, overWhich;
+	@SuppressWarnings("unused")
+	private Ani ani;
 	
 	private final static int width = 1200, height = 650;
 	
 	public void setup() {
-
 		size(width, height);
 		smooth();
 		loadData();
 		loadNodeAndLink();
-		
+		Ani.init(this);
 	}
 
 	public void draw() {
-		// Draw the background and show the nodes.
+		// Draw the background.
 		background(255);
 		for (Character c : characters) {
+			// Display the nodes first, then test if the mouse is pointing to any of them.
+			// If the mouse is pointing to one of them, assign the pointer to "overWhich",
+			// else move back the node.
 			c.display();
+			if (dist(c.x, c.y, mouseX, mouseY) < Character.DIAMETER / 2 && !mousePressed) {
+				overWhich = c;
+			} else {
+				ani = Ani.to(c, (float) 0.5, "diameter", Character.DIAMETER);
+			}
 		}
-		for (Character c : characters) {
-			c.showLabelAndBigger();
+		// If overWhich is not null, make it bigger and draw the label.
+		if (overWhich != null) {
+			if (dist(overWhich.x, overWhich.y, mouseX, mouseY) < Character.DIAMETER / 2) {
+				ani = Ani.to(overWhich, (float) 0.5, "diameter", 50);
+				fill(0, 162, 123);
+				rect(mouseX, mouseY -  15, overWhich.getName().length() * 15, 30, 15);
+				fill(255);
+				textSize(16);
+				text(overWhich.getName(), mouseX + 10, mouseY + 5);
+			}
+		}
+	}
+	
+	public void mousePressed() {
+		// When the mouse is pressed, lock the character which the mouse is pointing to.
+		if (overWhich != null) {
+			lockWhich = overWhich;
 		}
 	}
 
+	public void mouseDragged() {
+		// The locked character will follow the mouse.
+		if (lockWhich != null) {
+			lockWhich.x = mouseX;
+			lockWhich.y = mouseY;
+		}
+	}
+	
+	public void mouseReleased() {
+		// TODO: move to the big circle.
+		if (lockWhich != null) {
+			ani = Ani.to(lockWhich, (float) 0.5, "x", lockWhich.getOriginX());
+			ani = Ani.to(lockWhich, (float) 0.5, "y", lockWhich.getOriginY());
+		}
+	}
+	
 	public void keyPressed() {
 		// Change the versions.
 		if (keyCode == KeyEvent.VK_1) version = 1;
@@ -53,11 +96,12 @@ public class MainApplet extends PApplet{
 		else if (keyCode == KeyEvent.VK_5) version = 5;
 		else if (keyCode == KeyEvent.VK_6) version = 6;
 		else if (keyCode == KeyEvent.VK_7) version = 7;
+		// Then load the nodes again.
 		loadNodeAndLink();
 	}
 	
 	private void loadData(){
-		// Load in all data.
+		// Load all the data.
 		for (int i = 1; i <= 7; i++) {
 			datas[i] = loadJSONObject(path + fileFront + Integer.toString(i) + fileBack);
 			nodes[i] = datas[i].getJSONArray("nodes");
@@ -66,7 +110,9 @@ public class MainApplet extends PApplet{
 	}
 	
 	private void loadNodeAndLink() {
+		// Clear the list first.
 		characters.clear();
+		// Initialize the positions.
 		int x = 0, y = 0;
 		for (int i = 0; i < nodes[version].size(); i++) {
 			// When the node reaches the end, it will go back to the top.
@@ -74,12 +120,12 @@ public class MainApplet extends PApplet{
 				x = 35 + i * 5;
 				y = 48;
 			}
+			// Get the JSON object, add the information of the node, put it into the array.
 			JSONObject obj = nodes[version].getJSONObject(i);
-			Character c = new Character(this, obj.getString("name"), obj.getString("colour"), x, y, false, false, false);
+			Character c = new Character(this, obj.getString("name"), obj.getString("colour"), (float) x, (float) y);
 			characters.add(c);
 			// The gap between the nodes.
 			y += 55;
 		}
 	}
-
 }
